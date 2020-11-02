6672d2288b968653bd89c97bb93d8e94a950e04f
n/a (U118679) <n/a>
2020-11-02 15:23:47 +0100

    dini auti

---
+++ precache-manifest.df3668245f98e7c82287056dbb12408e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031ecf8c70597233aec1",
+    "url": "css/app.1975297b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4726a9a089a118465cc34e6c1698a35f",
+    "url": "index.html"
+  },
+  {
+    "revision": "031ecf8c70597233aec1",
+    "url": "js/app.49277859.js"
+  },
+  {
+    "revision": "375a4a63892cbe4367bb",
+    "url": "js/chunk-vendors.98489d15.js"
+  },
+  {
+    "revision": "f7aa4c46a2acbfd0bec600df9feab63b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
